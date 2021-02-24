b75af1b8b03796fb1d11cb29f80eb08a7ee34efd
illustriousgeorge <georgios.hraba@educanet.cz>
2021-02-24 12:54:54 +0100

    Initial commit

---
+++ src/main/java/cz/educanet/tranformations/logic/ScreenManager.java
@@ -0,0 +1,37 @@
+package cz.educanet.tranformations.logic;
+
+import cz.educanet.tranformations.logic.models.Coordinate;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ScreenManager {
+
+    private Set<Coordinate> selectedPoints = new HashSet<>();
+
+    public void select(Coordinate coordinate) {
+        selectedPoints.add(coordinate);
+    }
+
+    public void unselect(Coordinate coordinate) {
+        selectedPoints.remove(coordinate);
+    }
+
+    public boolean isSelected(Coordinate coordinate) {
+        return selectedPoints.contains(coordinate);
+    }
+
+    public Set<Coordinate> getSelectedPoints() {
+        return selectedPoints;
+    }
+
+    public boolean isFilledIn(Coordinate coordinate) {
+
+        int pixels = SCREEN_WIDTH * SCREEN_HEIGHT
+        if (screen1 == pixels) {
+            screen1.replace(screen2);
+        }
+    }
+
+}
+}
